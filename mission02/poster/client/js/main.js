6ac6d6469468c0fa73364d5948602229019c50d8
JAM <99.1.some@gmail.com>
2024-06-30 19:34:22 +0900

    이미지 변경까지

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,34 @@
 
 */
 
+// const navi = document.querySelector(".nav");
+const ulNavi = document.querySelector("ul");
+const nowImage = document.querySelector(".visual img");
 
+function setBgColor(node, color) {
+  node = document.querySelector(node);
+}
 
+function handleclick(e) {
+  const target = e.target.closest("li");
+  if (!target) return;
 
+  //인덱스, 컬러값 가져오기
+  const targetIndex = target.dataset.index-1;
+  const targetColor = `linear-gradient(to bottom, ${
+    data[targetIndex].color[0]
+  }, ${data[targetIndex].color[1]})`;
 
+  //누른 것만 활성 클래스 넣기
+  // const liItem = navi.querySelectorAll("li");
+  const liItem = [...ulNavi.children];
+  liItem.forEach((item) => item.classList.remove("is-active"));
+  target.classList.add("is-active");
 
+  //배경색 바뀌게 하기
+  document.body.style.background = targetColor;
 
-
-
-
-
-
-
-
+  //이미지 바뀌게 하기
+  nowImage.src = `./assets/${data[targetIndex].name.toLowerCase()}.jpeg`
+}
+ulNavi.addEventListener("click", handleclick);
